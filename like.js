40b9375ce73c37e76681942641e7efdceca57a9b
anshikagupta-04 <pg8998211@gmail.com>
2023-11-18 21:02:56 +0530

    first commit

---
+++ like.js
@@ -0,0 +1,16 @@
+var photo = document.querySelector(".photo")
+var like = document.querySelector("#like")
+var like_btn = document.querySelector("#btn")
+photo.addEventListener("dblclick", function(){
+    like.style.transform = 'translate(-50vw, -50vw) scale(1)';
+    like.style.opacity = 0.9;
+    setTimeout(function(){
+        like_btn.style.color = 'red'
+    },300)
+    // setTimeout(function(){
+    //     like.style.opacity = 0;
+    // }, 1000)
+    setTimeout(function(){
+        like.style.transform = 'translate(-50%, -50%) scale(0)'
+    },1000)
+})
